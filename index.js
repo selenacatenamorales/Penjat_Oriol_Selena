a19f5b49aa26e8e24b25ddd231a7553920935290
SELENA <catena.morales.selena@alumnat.copernic.cat>
2020-10-28 17:52:49 +0100

    SCM - random de listado de palabras, solucionado error apertura paginas

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 window.open("pagina_secundaria.html", "dibuix", "left=100px, height=50px, resizable=false");
-window.open("pagina_terciaria.html", "dibuix", "left=100px, height=50px, resizable=false");
-window.open("pagina_cuaternaria.html", "dibuix", "left=100px, height=50px, resizable=false");
+window.open("pagina_terciaria.html", "palabra", "left=100px, height=50px, resizable=false");
+window.open("pagina_cuaternaria.html", "estadisticas", "left=100px, height=50px, resizable=false");
+
 var palabras = prompt("Indica las palabras separadas por coma (',')");
 console.log(palabras);
+
+listadoPalabras = palabras.trim().split([","]);
+console.log(listadoPalabras);
+
+var palabraelegida = listadoPalabras[Math.floor(Math.random() * listadoPalabras.length)];
+console.log(palabraelegida);
