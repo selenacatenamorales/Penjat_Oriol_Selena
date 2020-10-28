7f4b17e0156e6bf70a38569c8e3a03cd06c1a3f7
SELENA <catena.morales.selena@alumnat.copernic.cat>
2020-10-28 17:41:39 +0100

    SCM - prompt inicial

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 window.open("pagina_secundaria.html", "dibuix", "left=100px, height=50px, resizable=false");
 window.open("pagina_terciaria.html", "dibuix", "left=100px, height=50px, resizable=false");
 window.open("pagina_cuaternaria.html", "dibuix", "left=100px, height=50px, resizable=false");
+var palabras = prompt("Indica las palabras separadas por coma (',')");
+console.log(palabras);
