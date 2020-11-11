c290d5273194bded1d117704f024c251af24d9e9
unknown <you@example.com>
2020-11-11 19:54:46 +0100

    ol-contadores quasi fets

--- index.js
+++ index.js
@@ -91,7 +91,8 @@ function novaparaula_temps(){
   alert("Espera mentres preparem la nova paraula");
   setTimeout(function(){ alert("Gràcies per esperar-te 5 segons"); }, 5000); //setTimeout executa l’expressió passats msec mil·lisegons.
   setCookie("PartidasA", partidasA, 10)                                        //L'usuari ha d'esperar-se al passar 5 segons si vol tornar a començar el joc
-  novaparaula();                                                                          
+  novaparaula();   
+  pagina_cuaternaria.document.getElementById("partidesA").innerText = "Partides abandonades: " + partidasA;                                                                       
 }
 
 function novaparaula(){
@@ -154,7 +155,8 @@ function introduirLletra(){
                 pagina_terciaria.document.getElementById("lletres").innerText = "HAS PERDUT";
                 alert("Espera mentres preparem la nova paraula, més sort la proxima vegada");
                 setTimeout(function(){alert("Gràcies per esperar-te 10 segons")}, 10000);
-                novaparaula()
+                novaparaula();
+                pagina_cuaternaria.document.getElementById("partidesP").innerText = "Partides perdudes: " + partidasP;
                 break;
     }
     
@@ -193,6 +195,5 @@ for(i=0; i<palabraD.length; i++){   //funcio per mostrar l'array de la paraula q
  // if(a == b);
 //}
 
-//pagina_cuaternaria.document.getElementById("partidesG").innerText = "Partides guanyades: 1232323";
-//pagina_cuaternaria.document.getElementById("partidesP").innerText = "Partides guanyades: 1232323";
-//pagina_cuaternaria.document.getElementById("partidesA").innerText = "Partides guanyades: 1232323";
+pagina_cuaternaria.document.getElementById("partidesG").innerText = "Partides guanyades: " + partidasG;
+
